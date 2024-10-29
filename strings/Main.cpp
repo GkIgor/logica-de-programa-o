10572805296c9167b1c700c42ddc6b8dd46dab59
GkIgor <igorf0660@gmail.com>
2024-10-29 02:26:22 -0300

    adicionando comentario e formatando o codigo

--- strings/Main.cpp
+++ strings/Main.cpp
@@ -1,18 +1,17 @@
-#include <iostream>
-#include "strings.cpp"
+#include <iostream> // Biblioteca de entrada e saida
+#include "strings.cpp" // Arquivo com o namespace $strings
 
-using $strings::size;
-using $strings::toUppercase;
-using std::cout;
-using std::endl;
+using $strings::size; // Importa a funcionalidade de $strings::size
+using $strings::toUppercase; // Importa a funcionalidade de $strings::toUppercase
+using std::cout; // Importa a funcionalidade de std::cout
+using std::endl; // Importa a funcionalidade de std::endl
 
-int main(int argc, char const *argv[])
+int main(int argc, char const *argv[]) // Entrypoint do programa
 {
 
-  for (int i = 1; i <= argc - 1; i++)
+  for (int i = 1; i <= argc - 1; i++) // Percorre os argumentos da linha de comando
   {
-
-    cout << toUppercase((char *)argv[i]) << endl;
+    cout << toUppercase( (char *)argv[i] /* Cast argv[i] para char */ ) << endl; // Imprime a string em maiúsculo
   }
 
   return 0;
